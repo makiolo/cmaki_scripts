66b9937e14f2b921cfd0c7d95155ea6c70a819bc
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-09 01:54:54 +0200

    .

--- cmaki.js
+++ cmaki.js
@@ -35,9 +35,9 @@ var dir_script;
 var script = process.argv[2];
 if (is_win)
 {
-	if(fs.existsSync(path.join(process.env['CMAKI_PWD'], script+".cmd")))
+	if(fs.existsSync(path.join(process.cwd(), script+".cmd")))
 	{
-		dir_script = process.env['CMAKI_PWD'];
+		dir_script = process.cwd();
 		console.log("1. dir_script == " + dir_script);
 	}
 	else
@@ -51,9 +51,9 @@ else
 	console.log("env CMAKI_PWD = " + process.env['CMAKI_PWD']);
 	console.log("cwd = " + process.cwd());
 	console.log("__dirname = " + __dirname);
-	if(fs.existsSync(path.join(process.env['CMAKI_PWD'], script+".sh")))
+	if(fs.existsSync(path.join(process.cwd(), script+".sh")))
 	{
-		dir_script = process.env['CMAKI_PWD'];
+		dir_script = process.cwd();
 		console.log("3. dir_script == " + dir_script);
 	}
 	else
