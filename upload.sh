89ae26b48faae5f9fe4f240d0880ce11abb57576
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-09 09:49:16 +0200

    .

--- upload.sh
+++ upload.sh
@@ -2,6 +2,7 @@
 export CMAKI_PWD=$(pwd)
 export PACKAGE=$(basename $CMAKI_PWD)
 export YMLFILE=$CMAKI_PWD/cmaki.yml
+export MODE="${MODE:-Debug}"
 
 cd $(pwd)/node_modules/cmaki_generator
 ./build ${PACKAGE} --yaml=${YMLFILE} --server=http://artifacts.myftp.biz:8080
