90bd51c1e19ad0d477dea21e9f865f80f331ebf1
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-09 10:04:31 +0200

    .

--- clean.sh
+++ clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-rm -Rf node_modules/depends 2> /dev/null
+export NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
+if [ "$NOCACHE_LOCAL" == "TRUE" ]; then
+	rm -Rf node_modules/depends 2> /dev/null
+fi
 rm -Rf coverage 2> /dev/null
 rm -Rf gcc 2> /dev/null
 rm -Rf clang 2> /dev/null
+
