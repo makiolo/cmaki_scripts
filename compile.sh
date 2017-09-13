6a5ac58a44f890a403dda5f3d12b588d46380ea7
Ricardo <makiolo@gmail.com>
2017-09-13 19:04:34 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,12 +4,13 @@ NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
 export CC="${CC:-gcc}"
 export CXX="${CXX:-g++}"
 export MODE="${MODE:-Debug}"
+export COMPILER_BASENAME=$(basename ${CC})
 
-echo "running in mode $MODE ... ($CC / $CXX)"
-cd $CC/$MODE
+echo "running in mode $MODE ... ($COMPILER_BASENAME)"
+cd $COMPILER_BASENAME/$MODE
 
 # CORES=$(grep -c ^processor /proc/cpuinfo)
-CORES=8
+CORES=12
 cmake --build . --config $MODE --target install -- -j$CORES -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
 code=$?
 exit $code
