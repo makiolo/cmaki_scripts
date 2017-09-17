877837ffd013a96677494cc9f4051b5aaaaef823
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-17 16:28:28 +0200

    .

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
 export CC="${CC:-gcc}"
 export CXX="${CXX:-g++}"
 export MODE="${MODE:-Debug}"
+export TARGET="${TARGET:-install}"
 export COMPILER_BASENAME=$(basename ${CC})
 
 echo "running in mode $MODE ... ($COMPILER_BASENAME)"
@@ -11,6 +12,6 @@ cd $COMPILER_BASENAME/$MODE
 
 # CORES=$(grep -c ^processor /proc/cpuinfo)
 CORES=12
-cmake --build . --config $MODE --target install -- -j$CORES -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
+cmake --build . --config $MODE --target $TARGET -- -j$CORES -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
 code=$?
 exit $code
