19ed364f3d3a1e2467a46b548451bb26fbf766d4
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-17 16:32:28 +0200

    .

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
-
 export CMAKI_PWD="${CMAKI_PWD:-$PWD}"
-export MODE="${MODE:-Debug}"
-export CMAKI_IDENTIFIER_FOLDER=$CMAKI_PWD/cmaki_identifier
-export PACKAGE=$(basename $(pwd))
-
-# setup
 $CMAKI_PWD/node_modules/cmaki_scripts/setup.sh
-
-# compile
 $CMAKI_PWD/node_modules/cmaki_scripts/compile.sh
 
