ec631d5e52d5a534c2fbb4e425f9ccc714682072
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-17 19:26:25 +0200

    .

--- docker.sh
+++ docker.sh
@@ -3,9 +3,9 @@ export IMAGE="${IMAGE:-linux-x64}"
 export MODE="${MODE:-Debug}"
 
 git clone -q https://github.com/dockcross/dockcross.git
-sudo docker run --rm dockcross/$IMAGE > ./dockcross-$IMAGE
+docker run --rm dockcross/$IMAGE > ./dockcross-$IMAGE
 chmod +x ./dockcross-$IMAGE
-export CMAKI_EMULATOR=$(sudo ./dockcross-$IMAGE bash -c 'echo "message(\${CMAKE_CROSSCOMPILING_EMULATOR})" >> ${CMAKE_TOOLCHAIN_FILE} && cmake -P ${CMAKE_TOOLCHAIN_FILE}')
+export CMAKI_EMULATOR=$(./dockcross-$IMAGE bash -c 'echo "message(\${CMAKE_CROSSCOMPILING_EMULATOR})" >> ${CMAKE_TOOLCHAIN_FILE} && cmake -P ${CMAKE_TOOLCHAIN_FILE}')
 echo emulator for $IMAGE: $CMAKI_EMULATOR
-sudo ./dockcross-$IMAGE -a "-e CMAKI_EMULATOR=$CMAKI_EMULATOR -e MODE=$MODE -e NOCACHE_LOCAL=TRUE -e NOCACHE_REMOTE=FALSE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
+./dockcross-$IMAGE -a "-e CMAKI_EMULATOR=$CMAKI_EMULATOR -e MODE=$MODE -e NOCACHE_LOCAL=TRUE -e NOCACHE_REMOTE=FALSE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
 
