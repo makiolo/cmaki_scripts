79f31719d5a17c18b9817de44388edd7ff0278df
Ricardo <makiolo@gmail.com>
2017-09-18 01:16:43 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,52 +1,9 @@
 #!/bin/bash
 set -e
 
-echo [0/3] preinstall
-if [[ "$OSTYPE" =~ ^linux ]]; then
-	curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-	sudo apt install -y nodejs
-	sudo apt install -y lcov
-	sudo apt install -y cppcheck
-
-	# # cmake 3.5 from source
-	# sudo apt install -y g++
-	# wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
-	# tar -zxvf cmake-3.7.2.tar.gz
-	# if [[ -d "cmake-3.7.2" ]]; then
-	# 	rm -Rf "cmake-3.7.2"
-	# fi
-	# cd "cmake-3.7.2"
-	# CC=gcc CXX=g++ ./configure --prefix=/usr/local
-	# make -j16
-	# sudo make install
-	# cd ..
-
-	# cmake 3.5 precompiled
-	DEPS_DIR=$(pwd)/deps
-	if [[ -d "$DEPS_DIR" ]]; then
-		rm -Rf $DEPS_DIR
-	fi
-	CMAKE_FILE=cmake-3.5.2-Linux-x86_64.tar.gz
-	CMAKE_URL=http://www.cmake.org/files/v3.5/${CMAKE_FILE}
-	wget ${CMAKE_URL} --quiet --no-check-certificate
-	mkdir -p cmake
-	tar -xzf ${CMAKE_FILE} -C cmake --strip-components 1
-	mv cmake ${DEPS_DIR}
-	export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-	cmake --version
-else
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew update
-	brew doctor
-	export PATH="/usr/local/bin:$PATH"
-	brew install node
-	brew install cmake
-	brew install lcov
-	brew install cppcheck
-fi
-pip install --user pyyaml
-pip install --user poster
-pip install --user codecov
+# hacerlo si no esto dentro de un contenedor docker que incluye cmaki_depends
+# sería bueno tener una variable para indicar que el entorno tiene las "cmaki_depends"
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/cmaki_depends.sh | bash
 
 export NOCACHE_LOCAL="${NOCACHE_LOCAL:-TRUE}"
 export NOCACHE_REMOTE="${NOCACHE_REMOTE:-FALSE}"
@@ -94,4 +51,3 @@ if [ -f "cmaki.yml" ]; then
 		./node_modules/cmaki_scripts/upload.sh
 	fi
 fi
-
