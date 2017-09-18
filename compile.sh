c89bc8e617f28bab40530ea23a27cd309dfa80e5
Ricardo <makiolo@gmail.com>
2017-09-18 23:58:15 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ export NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
 export CC="${CC:-gcc}"
 export CXX="${CXX:-g++}"
 export MODE="${MODE:-Debug}"
-export TARGET="${TARGET:-install}"
+export CMAKI_TARGET="${CMAKI_TARGET:-install}"
 export COMPILER_BASENAME=$(basename ${CC})
 
 echo "running in mode $MODE ... ($COMPILER_BASENAME)"
@@ -12,6 +12,6 @@ cd $COMPILER_BASENAME/$MODE
 
 # CORES=$(grep -c ^processor /proc/cpuinfo)
 CORES=12
-cmake --build . --config $MODE --target $TARGET -- -j$CORES -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
+cmake --build . --config $MODE --target $CMAKI_TARGET -- -j$CORES -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
 code=$?
 exit $code
