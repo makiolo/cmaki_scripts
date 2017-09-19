447754272f42223218d881611df607d9134cb97c
Ricardo <makiolo@gmail.com>
2017-09-19 18:49:46 +0200

    Update upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -1,6 +1,4 @@
 @echo off
-echo APPVEYOR_REPO_NAME = %APPVEYOR_REPO_NAME%
 cd node_modules\cmaki_generator
-build github://makiolo/design-patterns-cpp14 -o --server=http://artifacts.myftp.biz:8080
+build --yaml=..\..\cmaki.yml --server=http://artifacts.myftp.biz:8080 -d
 cd ..
-
