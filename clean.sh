31c64cb77db186b86a5c412b3c02b8c0d1da4485
Ricardo <makiolo@gmail.com>
2017-09-20 00:44:45 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
 if [ "$NOCACHE_LOCAL" == "TRUE" ]; then
-	rm -Rf node_modules/depends 2> /dev/null
+	rm -Rf artifacts 2> /dev/null
 fi
 rm -Rf coverage 2> /dev/null
 rm -Rf gcc 2> /dev/null
 rm -Rf clang 2> /dev/null
-
