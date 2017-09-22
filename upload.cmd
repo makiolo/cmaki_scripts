64453f699157660f4795309bb7979a644dac308f
Ricardo <makiolo@gmail.com>
2017-09-22 18:07:48 +0200

    Update upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd node_modules\cmaki_generator
-build --yaml=..\..\cmaki.yml --server=http://artifacts.myftp.biz:8080 -d
+build --yaml=..\..\cmaki.yml --server=http://artifacts.myftp.biz:8080
 cd ..
