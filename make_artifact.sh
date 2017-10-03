661584f4871ccd8b4b3df79b55147c282e76b659
Ricardo <makiolo@gmail.com>
2017-10-03 13:03:04 +0200

    Create make_artifact.sh

---
+++ make_artifact.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MODE="${MODE:-Debug}"
+export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
+export PACKAGE="${PACKAGE:-undefined}"
+export YMLFILE="${YMLFILE:-undefined}"
+
+if [ "$YMLFILE" == "undefined" ]; then
+  if [ "$PACKAGE" == "undefined" ]; then
+    echo Error: must define env var YMLFILE or PACKAGE
+  else
+    ./build $PACKAGE --server=http://artifacts.myftp.biz:8080
+  fi
+else
+  ./build --yaml=${YMLFILE} --server=http://artifacts.myftp.biz:8080
+fi
