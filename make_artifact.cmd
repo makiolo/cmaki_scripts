b0ed784eb441da30e9cc86da013bcb15cbe020f1
Ricardo <makiolo@gmail.com>
2017-10-12 22:40:59 +0200

    Update make_artifact.cmd

--- make_artifact.cmd
+++ make_artifact.cmd
@@ -1,16 +1,16 @@
 @echo off
 
-IF DEFINED CMAKI_PWD (
-  set CMAKI_PWD=%CMAKI_PWD%
-) else (
-  set CMAKI_PWD=%CD%
-)
-
-IF DEFINED CMAKI_INSTALL (
-  set CMAKI_INSTALL=%CMAKI_INSTALL%
-) else (
-  set CMAKI_INSTALL=%CMAKI_PWD%/bin
-)
+:: IF DEFINED CMAKI_PWD (
+::   set CMAKI_PWD=%CMAKI_PWD%
+:: ) else (
+::   set CMAKI_PWD=%CD%
+:: )
+:: 
+:: IF DEFINED CMAKI_INSTALL (
+::   set CMAKI_INSTALL=%CMAKI_INSTALL%
+:: ) else (
+::   set CMAKI_INSTALL=%CMAKI_PWD%/bin
+:: )
 
 IF DEFINED MODE (
   set MODE=%MODE%
