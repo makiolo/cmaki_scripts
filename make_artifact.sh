311e5462e5e6e42b7e63769208c617893aa44f2f
Ricardo <makiolo@gmail.com>
2017-10-14 20:57:21 +0200

    Update make_artifact.sh

--- make_artifact.sh
+++ make_artifact.sh
@@ -9,7 +9,7 @@ if [ "$YMLFILE" == "undefined" ]; then
   if [ "$PACKAGE" == "undefined" ]; then
     echo Error: must define env var YMLFILE or PACKAGE
   else
-    ./build $PACKAGE --server=http://artifacts.myftp.biz:8080
+    ./build $PACKAGE --server=http://artifacts.myftp.biz:8080 -d
   fi
 else
   ./build --yaml=${YMLFILE} --server=http://artifacts.myftp.biz:8080
