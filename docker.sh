74e4f918c7ca240dae462191cea878da5f1f305b
Ricardo <makiolo@gmail.com>
2017-10-14 15:22:05 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@ export IMAGE="${IMAGE:-linux-x64}"
 export MODE="${MODE:-Debug}"
 export NOCACHE_REMOTE="${NOCACHE_REMOTE:-FALSE}"
 export NOCACHE_LOCAL="${NOCACHE_LOCAL:-TRUE}"
-export DOCKER_OWNER=dockcross
+export DOCKER_OWNER=makiolo
 
 docker run --rm $DOCKER_OWNER/$IMAGE > ./dockcross-$IMAGE
 chmod +x ./dockcross-$IMAGE
-./dockcross-$IMAGE -a "-e MODE=$MODE -e NOCACHE_LOCAL=$NOCACHE_LOCAL -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=TRUE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
+./dockcross-$IMAGE -a "-e MODE=$MODE -e NOCACHE_LOCAL=$NOCACHE_LOCAL -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=FALSE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
